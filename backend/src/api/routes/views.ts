af82834aaccbed49253cfdfc9563ffbdae17d6f9
Minwoo Kim <mvvkinn@mvvkinn.xyz>
2022-11-08 00:44:55 +0900

    Make vue index to express index view

--- backend/src/api/routes/views.ts
+++ backend/src/api/routes/views.ts
@@ -3,13 +3,7 @@ import { Router } from "express";
 const route = Router();
 
 export default (app: Router) => {
-  app.use(route);
-
-  app.get("/", (req, res) => {
-    res.render("index.ejs");
-  });
-
-  app.get("/signupPage", (req, res) => {
-    res.render("signup.ejs");
+  app.get("/", (req, res, next) => {
+    res.render("index");
   });
 };
--- backend/src/loaders/express.ts
+++ backend/src/loaders/express.ts
@@ -19,8 +19,7 @@ export default (app: express.Application) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
-  app.set("view engine", "ejs");
-  app.set("views", __dirname + "/../views");
+  app.use(express.static(__dirname + "/../public"));
 
   app.use(routes());
 
