7dc23b1db4b9fb4c79c6836a6ff42f5648d34727
Minwoo Kim <mvvkinn@mvvkinn.xyz>
2022-11-11 12:02:57 +0900

    Add dotenv and default config

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,5 +1,14 @@
+import dotenv from "dotenv";
+
+// Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
+const envFound = dotenv.config();
+if (envFound.error) {
+  // This error should crash whole process
+
+  throw new Error("Couldn't find src/.env");
+}
 export default {
   db:
     process.env.NODE_ENV == "development"
