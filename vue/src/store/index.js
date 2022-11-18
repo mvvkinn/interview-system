ceace6dabf5aa10f1fc80f00c86a81156e4bf5e7
taemin-Jang <jtm990506@gmail.com>
2022-11-19 00:26:51 +0900

    Move login.js to index.js, Fix login logic, Add loginUser

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -2,23 +2,75 @@
 
 import Vue from "vue";
 import Vuex from "vuex";
-import { login } from "./module/login";
+import axios from "axios";
+import VueCookies from "vue-cookies";
+import router from "@/router";
+
+// 쿠키를 사용한다.
+Vue.use(VueCookies);
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   // 여러 컴포넌트에서 공유할 공동의 상태
-  state: {},
+  state: {
+    Token: null,
+    user: {
+      address: null,
+      birthdate: null,
+      email: null,
+      gender: null,
+      name: null,
+      phone: null,
+      recieve_info: null,
+      role: null,
+      username: null,
+      zipcode: null,
+    },
+  },
+  mutations: {
+    // 로그인 토큰 쿠키에 저장, 1시간동안
+    loginToken(state, payload) {
+      VueCookies.set("Token", payload, "1h");
+      state.Token = payload;
+    },
 
-  // state를 변경시키기 위한 유일한 방법 (동기적 로직)
-  mutations: {},
+    // 쿠키에 저장되어있는 토큰 제거
+    removeToken() {
+      VueCookies.remove("Token");
+    },
 
-  // 공동의 상태를 계산하여 state의 값을 반환
-  getters: {},
-
-  // 비동기적 로직
-  actions: {},
-  modules: {
-    login,
+    // 사용자 정보 state.user에 저장
+    loginUser(state, payload) {
+      state.user = payload;
+    },
+  },
+  getters: {
+    //쿠키에 저장된 토큰 가져오기
+    getToken() {
+      let Token = VueCookies.get("Token");
+      return {
+        Token: Token,
+      };
+    },
+  },
+  actions: {
+    login: async ({ commit }, params) => {
+      await axios
+        .post("/auth/signin", params)
+        .then((res) => {
+          commit("loginToken", res.data.token);
+          commit("loginUser", res.data.user);
+          router.push("/main");
+        })
+        .catch((err) => {
+          console.log(err.message);
+          return true;
+        });
+    },
+    logout: (context) => {
+      // 로그아웃
+      context.commit("removeToken");
+    },
   },
 });
