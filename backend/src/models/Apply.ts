5cb1bd4e8eb801d8134cffe0733e77a12124d309
Minwoo Kim <mvvkinn@mvvkinn.xyz>
2022-12-02 22:58:53 +0900

    Add Apply model

---
+++ backend/src/models/Apply.ts
@@ -0,0 +1,27 @@
+import { DataTypes, Sequelize } from "sequelize";
+import Container from "typedi";
+
+export default (db: Sequelize) => {
+  const applyModel = db.define(
+    "Apply",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        unique: true,
+        autoIncrement: true,
+      },
+      user_name: { type: DataTypes.STRING },
+      interview_number: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+      },
+      title: DataTypes.STRING,
+    },
+    { tableName: "Apply", timestamps: false }
+  );
+
+  return applyModel;
+};
