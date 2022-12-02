ae85b4fbc9c50945891efd2fb35aee728012723a
Minwoo Kim <mvvkinn@mvvkinn.xyz>
2022-12-02 22:22:26 +0900

    Add webRTC signaling server to dependency injector

--- backend/src/loaders/dependencyInjector.ts
+++ backend/src/loaders/dependencyInjector.ts
@@ -1,12 +1,19 @@
 import { Sequelize } from "sequelize";
 import { Container } from "typedi";
 import LoggerInstance from "./logger";
+import socketIOInstance from "./socket.io";
 import User from "@models/User";
+import { Server } from "http";
+import RTCSignalService from "@services/RTCSignalService";
 
-export default (sequelizeInstance: Sequelize) => {
+export default (sequelizeInstance: Sequelize, httpServer: Server) => {
   Container.set("logger", LoggerInstance);
 
   Container.set("db", sequelizeInstance);
 
   Container.set("userModel", User(sequelizeInstance));
+
+  Container.set("socketIO", socketIOInstance(httpServer));
+
+  RTCSignalService();
 };
