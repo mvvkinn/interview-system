ebbe95970e2781604519fb7b4606f57cdfc3af8e
LSeolAh <sualah0417@gmail.com>
2022-12-03 13:36:47 +0900

    Fix ApplyService create

--- backend/src/services/ApplyService.ts
+++ backend/src/services/ApplyService.ts
@@ -1,14 +1,31 @@
 import { Container } from "typedi";
 import { ModelCtor, Sequelize } from "sequelize";
+import { IApply } from "@interfaces/IApply";
+import { IUser, IUserSignInDTO } from "@interfaces/IUser";
 
 export default class ApplyService {
   applyModel: ModelCtor<any>;
+  userModel: ModelCtor<any>;
   constructor() {
     const db: Sequelize = Container.get("db");
     this.applyModel = db.models.Apply;
+    this.userModel = db.models.User;
   }
 
-  public async create(record: object) {
-    await this.applyModel.create({ ...record });
+  public async create(email: IUser) {
+    const userName = this.userModel.findAll({
+      attributes: ["name"],
+      where: { email: email },
+    });
+    const userID = this.userModel.findAll({
+      attributes: ["id"],
+      where: { email: email },
+    });
+
+    await this.applyModel.create({
+      ...email,
+      user_name: userName,
+      UserID: userID,
+    });
   }
 }
