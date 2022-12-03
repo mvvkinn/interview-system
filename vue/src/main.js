28d90229c90139dcc15684aaa3a3d3cb913ab37d
taemin-Jang <jtm990506@gmail.com>
2022-12-03 19:12:59 +0900

    Add socket global variable

--- vue/src/main.js
+++ vue/src/main.js
@@ -4,12 +4,20 @@ import router from "./router";
 import { store } from "./store";
 import axios from "axios";
 import VueCookies from "vue-cookies";
+import io from "socket.io-client";
 import loadScript from "vue-plugin-load-script";
 
 Vue.use(loadScript);
 
 // 쿠키를 사용한다.
 Vue.use(VueCookies);
+
+const socket = io("http://localhost:3000", {
+  // transports: ["websocket"],
+  // withCredentials: true.valueOf,
+});
+
+Vue.prototype.$socket = socket;
 
 Vue.prototype.$axios = axios;
 
@@ -19,5 +27,5 @@ new Vue({
   loadScript,
   router,
   store,
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount("#app");
