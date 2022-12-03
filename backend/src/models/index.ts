fb0f427ca6c3fc8ed08e35e77952ebbc3125b212
0uizi0 <0uizi0@naver.com>
2022-12-03 18:06:40 +0900

    Refactor model index

---
+++ backend/src/models/index.ts
@@ -0,0 +1,23 @@
+import userModel from "@models/User";
+import { DataTypes, Sequelize } from "sequelize";
+import { Logger } from "winston";
+import Container from "typedi";
+
+import User from "@models/User";
+import Score from "./Score";
+
+export default async ( sequelizeInstance :Sequelize) => {
+    const userModel = User(sequelizeInstance);
+    const scoreModel = Score(sequelizeInstance);
+    const logger: Logger = Container.get("logger");
+
+    userModel.hasMany(scoreModel,{
+        sourceKey: "id",
+        onDelete: "cascade",
+        onUpdate: "cascade",
+    });
+
+    scoreModel.belongsTo(userModel);
+
+    await sequelizeInstance.sync();
+}
