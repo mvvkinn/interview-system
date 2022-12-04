8a7225ee6971a0b99ab0e222ea3b56e264f3d647
taemin-Jang <jtm990506@gmail.com>
2022-12-04 19:00:17 +0900

    Add roomName, currentNum, user to roomObjArr

--- backend/src/services/RTCSignalService.ts
+++ backend/src/services/RTCSignalService.ts
@@ -6,12 +6,73 @@ export default () => {
   const logger: Logger = Container.get("logger");
   const io: Server<any> = Container.get("socketIO");
 
-  io.on("connection", socket => {
-    socket.on("joinRoom", (roomName: string) => {
-      socket.join(roomName);
+  interface User {
+    socketId: string;
+  }
 
+  interface TargetRoomObj {
+    roomName: string;
+    currentNum: number;
+    user: Array<User>;
+  }
+
+  let roomObjArr: any[] = [
+    // {
+    //   roomName,
+    //   currentNum,
+    //   user: [
+    //     {socketId}
+    //   ]
+    // },
+  ];
+  const MAXIMUM = 4;
+
+  io.on("connection", (socket) => {
+    let myRoomName = "";
+    socket.on("joinRoom", (roomName: string) => {
+      myRoomName = roomName;
+      let isRoomExist = false;
+      let targetRoomObj: TargetRoomObj = {
+        roomName,
+        currentNum: 0,
+        user: [],
+      };
+
+      for (let i = 0; i < roomObjArr.length; ++i) {
+        if (roomObjArr[i].roomName === roomName) {
+          // Reject join the room
+          if (roomObjArr[i].currentNum >= MAXIMUM) {
+            socket.emit("reject_join");
+            return;
+          }
+
+          isRoomExist = true;
+          targetRoomObj = roomObjArr[i];
+          console.log(targetRoomObj);
+          break;
+        }
+      }
+
+      // Create room
+      if (!isRoomExist) {
+        targetRoomObj = {
+          roomName,
+          currentNum: 0,
+          user: [],
+        };
+        roomObjArr.push(targetRoomObj);
+      }
+
+      //Join the room
+      targetRoomObj.user.push({
+        socketId: socket.id,
+      });
+      ++targetRoomObj.currentNum;
+
+      console.log(targetRoomObj);
+      socket.join(roomName);
       // emitting event after join room
-      socket.to(roomName).emit("join");
+      socket.to(roomName).emit("join", targetRoomObj.user);
     });
 
     socket.on("offer", (offer: object, roomName: string) => {
