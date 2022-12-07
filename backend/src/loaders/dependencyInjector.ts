a50aedd82141f618d922b899c63ece05b04ddc32
taemin-Jang <jtm990506@gmail.com>
2022-12-07 22:51:44 +0900

    Add socket.io container set and RTCSignalService

--- backend/src/loaders/dependencyInjector.ts
+++ backend/src/loaders/dependencyInjector.ts
@@ -3,10 +3,11 @@ import { Container } from "typedi";
 import LoggerInstance from "./logger";
 import socketIOInstance from "./socket.io";
 import Apply from "@models/Apply";
-import { Server } from "http";
 import models from "@models/index";
 import Score from "@models/Score";
 import User from "@models/User";
+import { Server } from "http";
+import RTCSignalService from "@services/RTCSignalService";
 
 export default async (sequelizeInstance: Sequelize, httpServer: Server) => {
   Container.set("logger", LoggerInstance);
@@ -15,9 +16,10 @@ export default async (sequelizeInstance: Sequelize, httpServer: Server) => {
 
   models(sequelizeInstance);
 
+  Container.set("scoreModel", Score(sequelizeInstance));
   Container.set("applyModel", Apply(sequelizeInstance));
   Container.set("userModel", User(sequelizeInstance));
 
-  Container.set("scoreModel", Score(sequelizeInstance));
-  Container.set("userModel", User(sequelizeInstance));
+  Container.set("socketIO", socketIOInstance(httpServer));
+  RTCSignalService();
 };
