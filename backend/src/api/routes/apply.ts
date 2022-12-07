3e3b8748ef195dbb5d50abd35f1edcd0d3bbd7ef
LSeolAh <sualah0417@gmail.com>
2022-12-08 05:08:31 +0900

    Fix ApplyService

--- backend/src/api/routes/apply.ts
+++ backend/src/api/routes/apply.ts
@@ -1,4 +1,5 @@
 import { IUser } from "@interfaces/IUser";
+import { IApply } from "@interfaces/IApply";
 import ApplyService from "@services/ApplyService";
 import { Router, Request, Response, NextFunction } from "express";
 import { ModelCtor } from "sequelize";
@@ -22,9 +23,10 @@ export default (app: Router) => {
       const email: IUser = req.body.email;
       const name: IUser = req.body.name;
       const id: IUser = req.body.id;
+      const title: IApply = req.body.title;
 
       const applyServiceInstance = new ApplyService();
-      const result = await applyServiceInstance.create(email, name, id);
+      const result = await applyServiceInstance.create(email, name, id, title);
 
       res.status(201).send(result);
     } catch (e) {
