3a96697b610a744422a39505243e22456e0f89c8
LSeolAh <sualah0417@gmail.com>
2022-12-12 22:20:59 +0900

    Fix applymodel

--- backend/src/models/Apply.ts
+++ backend/src/models/Apply.ts
@@ -5,22 +5,18 @@ export default (db: Sequelize) => {
   const applyModel = db.define(
     "Apply",
     {
-      id: {
+      interview_number: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         unique: true,
         autoIncrement: true,
       },
+      qrcode: { type: DataTypes.STRING },
+      date: { type: DataTypes.INTEGER },
       user_name: { type: DataTypes.STRING },
-      interview_number: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        unique: true,
-      },
-      title: DataTypes.STRING,
-      user_id: { type: DataTypes.INTEGER },
-      token: { type: DataTypes.STRING },
+      notice_title: { type: DataTypes.STRING },
+      resume_id: { type: DataTypes.INTEGER },
     },
     { tableName: "Apply", timestamps: false }
   );
