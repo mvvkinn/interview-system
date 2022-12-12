21a8c9befcfeb5fd6ddafdc9a9556b395a0e071a
LSeolAh <sualah0417@gmail.com>
2022-12-13 00:18:41 +0900

    Fix ApplyService

--- backend/src/services/ApplyService.ts
+++ backend/src/services/ApplyService.ts
@@ -2,35 +2,50 @@ import { Container } from "typedi";
 import { ModelCtor, Sequelize } from "sequelize";
 import { IApply } from "@interfaces/IApply";
 import { IUser, IUserSignInDTO } from "@interfaces/IUser";
+import { IResume } from "@interfaces/IResume";
+import { INotice } from "@interfaces/INotice";
 
 export default class ApplyService {
   applyModel: ModelCtor<any>;
   userModel: ModelCtor<any>;
+  noticeModel: any;
   constructor() {
     const db: Sequelize = Container.get("db");
     this.applyModel = db.models.Apply;
     this.userModel = db.models.User;
+    this.noticeModel = db.models.Notice;
   }
 
   public async create(
-    email: IUser,
-    name: IUser,
-    id: IUser,
-    title: string,
-    title_id: string
+    user_email: IApply,
+    resume_id: IResume,
+    notice_id: INotice
   ) {
+    const userRecord = await this.userModel.findOne({
+      where: { email: user_email },
+    });
+    const user_id = userRecord.dataValues.id;
+    const user_name = userRecord.dataValues.name;
+
+    const noticeRecord = await this.noticeModel.findOne({
+      where: { id: notice_id },
+    });
+    const notice_title = noticeRecord.dataValues.title;
+
     const today = new Date();
     const year = today.getFullYear();
-    //19 = 임의의 Announcement.id(면접공고 id)
-    const array = [year, title_id, id];
+
+    const array = [year, notice_id, user_id];
     const interviewNumber = array.join("");
 
-    await this.applyModel.create({
-      ...email,
+    return await this.applyModel.create({
+      ...user_email,
       interview_number: interviewNumber,
-      user_id: id,
-      user_name: name,
-      title: title,
+      date: today,
+      user_name: user_name,
+      notice_title: notice_title,
+      resume_id: resume_id,
+      user_email: user_email,
     });
   }
 
