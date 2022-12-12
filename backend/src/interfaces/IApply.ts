f5e02310fc9df138c0df36b1443cbbcda88f128d
LSeolAh <sualah0417@gmail.com>
2022-12-13 00:18:02 +0900

    Fix apply interface

--- backend/src/interfaces/IApply.ts
+++ backend/src/interfaces/IApply.ts
@@ -1,8 +1,9 @@
 export interface IApply {
   interview_number: number;
   qrcode?: string;
-  date?: string;
+  date?: Date;
   user_name?: string;
+  user_email?: string;
   notice_title?: string;
   resume_id?: number;
 }
